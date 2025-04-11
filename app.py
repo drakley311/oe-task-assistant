578e1d0bcfa9603156cc7c86f266dec736880e96
drakley311 <bret.naugle@spreetail.com>
2025-04-11 15:13:10 -0400

    Update app.py with PORT

--- app.py
+++ app.py
@@ -67,4 +67,7 @@ def oauth_callback():
     )
     session["ms_token"] = token
     return redirect(url_for("home"))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
